f1feb1a8b32da5b4cee6c328036750d4422351d3
jiratQ <jirat123@icloud.com>
2022-05-10 14:46:45 +0700

    bug enermy position bug

--- monster.py
+++ monster.py
@@ -5,7 +5,7 @@ import time
 
 
 class Monster():
-    element = ["fire", "forest", "water" ]
+    element = ["fire", "forest", "water"]
     monsterimg = pygame.transform.scale(
         pygame.image.load("photo/monster.png"), (scale(158), scale(108)))
     elementimg = {}
@@ -23,26 +23,28 @@ class Monster():
         self.atk = random.randint(1, 10)
         self.hp = random.randint(1, 10)
         self.id = id
-        self.speed = scale(random.randint(2, 3))
+        self.speed = random.randint(2, 3)
         self.time = times
         self.nowtime = times-serverstarttime
         self.nowdifficulty = int(self.nowtime//30) + 1
         self.weakskill = [
             [Monster.element[random.randint(0, 2)] for i in range(3)]for i in range(self.nowdifficulty)]
         self.x = width*1.5
-        self.y = height//2
+        self.y = height//2-150
         self.tempx = self.x
+        self.tempy = self.y
 
     def update(self, deltatime):
-        self.tempx = self.x - self.speed * \
-            (time.time()-deltatime-self.time) * 60
+        self.tempx = scale(self.x - self.speed *
+                           (time.time()-deltatime-self.time) * 60)
+        self.tempy = scale(self.y)
 
     def draw(self, win):
-        win.blit(Monster.monsterimg, (self.tempx, self.y))
+        win.blit(Monster.monsterimg, (self.tempx, self.tempy))
         self.drawweak(win)
 
     def drawweak(self, win):
         for row in range(len(self.weakskill)):
             for index in range(len(self.weakskill[row])):
                 win.blit(Monster.elementimg[self.weakskill[row][index]],
-                         (self.tempx + scale(40+25*index), self.y-scale(40*row+40), scale(20), scale(20)))
+                         (self.tempx + scale(40+25*index), self.tempy-scale(40*row+40), scale(20), scale(20)))
